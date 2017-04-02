c085896bef3e6ae04c576583036ee2ee69d38f89
PhantomMike <mikesch@msn.com>
2017-04-02 10:58:14 -0700

    Fix apostrophe/single quote problem so date renders for jscript

--- script.js
+++ script.js
@@ -3,12 +3,21 @@ const jsContainer = document.getElementById("js");
 const reactContainer = document.getElementById("react");
 //const date = new Date();
 
-console.log(date);
+//console.log(date);
 
 const render = () => {
 
-jsContainer.innerHTML = '<div class="demo">Hello JS<input /><p>${new Date()}</p></div>';
+// This is what I typed in, but I had to use the single quotes from the commented out
+// line below which was from the sample itself. Couldn't use single quote (I guess
+// it is the apostrophe) from keyboard, weird!  Need to use the single quote generated
+// by the tilde key!!
+jsContainer.innerHTML = `<div class="demo">Hello JS<input /><p>${new Date()}</p></div>`;
+/*jsContainer.innerHTML = `
+<div class="demo">
+Hello JS<input />
+<p>${new Date()}</p></div>`;
 
+*/
 ReactDOM.render(
     React.createElement(
       "div",
